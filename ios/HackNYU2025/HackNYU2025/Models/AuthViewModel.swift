75a1a5035649f7f4bb764b4b5a53071c5ea7575c
Alec <alecagayan24@gmail.com>
2025-02-08 12:30:08 -0500

    authentication w/ firebase

---
+++ ios/HackNYU2025/HackNYU2025/Models/AuthViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  AuthViewModel.swift
+//  HackNYU2025
+//
+//  Created by Alec Agayan on 2/8/25.
+//
+
+import SwiftUI
+
+struct AuthViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthViewModel()
+}
---
+++ ios/HackNYU2025/HackNYU2025/Views/AuthView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthView.swift
+//  HackNYU2025
+//
+//  Created by Alec Agayan on 2/8/25.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthView()
+}
