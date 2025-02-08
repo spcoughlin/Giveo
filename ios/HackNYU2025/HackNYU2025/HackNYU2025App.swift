86016690cb752ea13546149016aaff25341b8ee4
Alec <alecagayan24@gmail.com>
2025-02-08 11:40:56 -0500

    initial app commit

---
+++ ios/HackNYU2025/HackNYU2025/HackNYU2025App.swift
@@ -0,0 +1,17 @@
+//
+//  HackNYU2025App.swift
+//  HackNYU2025
+//
+//  Created by Alec Agayan on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct HackNYU2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/HackNYU2025/HackNYU2025Tests/HackNYU2025Tests.swift
@@ -0,0 +1,17 @@
+//
+//  HackNYU2025Tests.swift
+//  HackNYU2025Tests
+//
+//  Created by Alec Agayan on 2/8/25.
+//
+
+import Testing
+@testable import HackNYU2025
+
+struct HackNYU2025Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
